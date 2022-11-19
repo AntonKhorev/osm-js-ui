0af3f477ac2c9d31820886680121f6ff6b37bc52
Anton Khorev <tony29@yandex.ru>
2022-11-19 14:35:41 +0300

    Add crosshair

--- build.js
+++ build.js
@@ -6,11 +6,42 @@ import typescript from '@rollup/plugin-typescript'
 await fs.rm('dist',{recursive: true, force: true})
 await fs.mkdir('dist')
 
-// copy files
-await fs.copyFile('src/index.html','dist/index.html')
-await fs.copyFile('src/index.css','dist/index.css')
-
 {
+	// process svgs
+	let embeddedStyles=''
+	let embeddedSymbols=''
+	for (const dirEntry of await fs.readdir('src/svg',{withFileTypes:true})) {
+		if (dirEntry.isDirectory()) continue
+		const filename=dirEntry.name
+		if (filename=='favicon.svg') continue
+		const [style,symbol]=getEmbeddedSvg(
+			filename.split('.')[0],
+			await fs.readFile(`src/svg/${filename}`,'utf-8')
+		)
+		embeddedStyles+=style
+		embeddedSymbols+=symbol
+	}
+
+	// build index with embedded svgs
+	const embeddedSvgs=
+		`<svg class="symbols">\n`+
+		`<style>\n`+
+		embeddedStyles+
+		`</style>\n`+
+		embeddedSymbols+
+		`</svg>`
+	// const favicon=await fs.readFile(`src/svg/favicon.svg`,'utf-8')
+	// const encodedFavicon=Buffer.from(favicon).toString('base64')
+	const htmlContents=await fs.readFile('src/index.html','utf-8')
+	const patchedHtmlContents=htmlContents
+		.replace(`<body>`,`<body data-build="${new Date().toISOString()}">`)
+		.replace(`<!-- {embed svgs} -->`,embeddedSvgs)
+		// .replace(`<!-- {embed favicon} -->`,`<link rel=icon href="data:image/svg+xml;charset=utf-8;base64,${encodedFavicon}">`)
+	await fs.writeFile('dist/index.html',patchedHtmlContents)
+}{
+	// copy css
+	await fs.copyFile('src/index.css','dist/index.css')
+}{
 	// compile and bundle scripts
 	const bundle=await rollup({
 		input: 'src/index.ts',
@@ -20,4 +51,27 @@ await fs.copyFile('src/index.css','dist/index.css')
 		file: "dist/index.js",
 	})
 	bundle.close()
+}
+
+function getEmbeddedSvg(id,input) {
+	let style=''
+	let symbol=''
+	for (const line of input.split(/\r?\n/)) {
+		let match
+		if (match=line.match(/^<svg.*(viewBox="[^"]*").*>$/)) {
+			const [,viewBox]=match
+			symbol+=`<symbol id="${id}" ${viewBox}>\n`
+		} else if (match=line.match(/^<\/svg>$/)) {
+			symbol+=`</symbol>\n`
+			break
+		} else if (match=line.match(/^<g class="([^"]*)">$/)) {
+			const [,partClass]=match
+			const visibility=(partClass=='default'?'visible':'hidden')
+			style+=`#${id} .${partClass} { visibility: var(--${id}-${partClass}-part-visibility,${visibility}); }\n`
+			symbol+=line+'\n'
+		} else {
+			symbol+=line+'\n'
+		}
+	}
+	return [style,symbol]
 }
--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@ main()
 async function main() {
 	const $map=document.createElement('div')
 	$map.classList.add('map')
+	const $crosshair=document.createElement('div')
+	$crosshair.classList.add('crosshair')
+	$crosshair.innerHTML=`<svg><use href="#map-crosshair" /></svg>`
+	$map.append($crosshair)
 	document.body.append($map)
 }
