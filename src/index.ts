3f6539e0c3c79254a488242fefe26d76e9836c5e
Anton Khorev <tony29@yandex.ru>
2022-11-20 17:23:14 +0300

    Enable mouse movement

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import {makeElement, makeEscapeTag} from './util'
 
 const makeDiv=makeElement('div')
 
+const tileSize=256
+const tileSizePow=8
+
 main()
 
 async function main() {
 	const e=makeEscapeTag(encodeURIComponent)
-	const tileSizeX=256
-	const tileSizeY=256
 	const initialZoom=17
 	const initialLat=59.93903
 	const initialLon=30.31582
@@ -22,6 +23,14 @@ async function main() {
 	$lat.value=String(initialLat)
 	const $lon=makeElement('input')()()
 	$lon.value=String(initialLon)
+
+	const updateInputs=()=>{
+		const [zoom,lat,lon]=calculateCoords(...position)
+		$zoom.value=String(zoom)
+		$lat.value=String(lat)
+		$lon.value=String(lon)
+	}
+
 	const $tiles=makeDiv('tiles')()
 	const $crosshair=makeDiv('crosshair')()
 	const $map=makeDiv('map')($tiles,$crosshair)
@@ -38,38 +47,37 @@ async function main() {
 	const replaceTiles=()=>{
 		$tiles.innerHTML=''
 		const [x,y,z]=position
-		const tileX=Math.floor(x)
-		const tileY=Math.floor(y)
-		const pixelX=Math.floor(x*tileSizeX)
-		const pixelY=Math.floor(y*tileSizeY)
-		const transX=-pixelX%tileSizeX
-		const transY=-pixelY%tileSizeY
+		const tileX=Math.floor(x/tileSize)
+		const tileY=Math.floor(y/tileSize)
+		const transX=-x%tileSize
+		const transY=-y%tileSize
 		const viewHalfSizeX=$map.clientWidth/2
 		const viewHalfSizeY=$map.clientHeight/2
 		const tileRange=Math.pow(2,z)
 		const tileMask=tileRange-1
 		const nExtraTilesXL=
-			Math.floor((viewHalfSizeX+transX)/tileSizeX)+1
+			Math.floor((viewHalfSizeX+transX)/tileSize)+1
 		const nExtraTilesXU=
-			Math.floor((viewHalfSizeX-transX)/tileSizeX)
+			Math.floor((viewHalfSizeX-transX)/tileSize)
 		const nExtraTilesYL=Math.min(
 			tileY,
-			Math.floor((viewHalfSizeY+transY)/tileSizeY)+1
+			Math.floor((viewHalfSizeY+transY)/tileSize)+1
 		)
 		const nExtraTilesYU=Math.min(
 			tileRange-tileY-1,
-			Math.floor((viewHalfSizeY-transY)/tileSizeY)
+			Math.floor((viewHalfSizeY-transY)/tileSize)
 		)
 		for (let iTileY=-nExtraTilesYL;iTileY<=nExtraTilesYU;iTileY++) {
 			for (let iTileX=-nExtraTilesXL;iTileX<=nExtraTilesXU;iTileX++) {
 				const tileUrl=e`https://tile.openstreetmap.org/${z}/${(tileX+iTileX)&tileMask}/${tileY+iTileY}.png`
 				const $img=document.createElement('img')
 				$img.src=tileUrl
-				$img.style.translate=`${transX+iTileX*tileSizeX}px ${transY+iTileY*tileSizeY}px`
+				$img.style.translate=`${transX+iTileX*tileSize}px ${transY+iTileY*tileSize}px`
 				$tiles.append($img)
 			}
 		}
 	}
+
 	$zoom.oninput=$lat.oninput=$lon.oninput=()=>{
 		position=calculatePosition(
 			parseInt($zoom.value,10),
@@ -78,14 +86,34 @@ async function main() {
 		)
 		replaceTiles()
 	}
+
 	const resizeObserver=new ResizeObserver(replaceTiles)
 	resizeObserver.observe($map)
+
+	$map.onmousemove=ev=>{
+		if (!(ev.buttons&1)) return
+		let [x,y,z]=position
+		x-=ev.movementX
+		y-=ev.movementY
+		position=[x,y,z]
+		updateInputs()
+		replaceTiles()
+	}
 }
 
 function calculatePosition(zoom:number,lat:number,lon:number):[x:number,y:number,z:number] {
 	return [
-		(lon+180)/360*Math.pow(2,zoom),
-		(1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2*Math.pow(2,zoom),
+		Math.floor(Math.pow(2,zoom+tileSizePow)*(lon+180)/360),
+		Math.floor(Math.pow(2,zoom+tileSizePow)*(1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2),
 		zoom
+	]
+}
+
+function calculateCoords(x:number,y:number,z:number):[zoom:number,lat:number,lon:number] {
+	const n=Math.PI-2*Math.PI*y/Math.pow(2,z+tileSizePow)
+	return [
+		z,
+		180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))),
+		x/Math.pow(2,z+tileSizePow)*360-180
 	]
 }
