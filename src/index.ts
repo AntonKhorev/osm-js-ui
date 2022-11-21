79b1af7c33bdabbbe4d66fd570282dd0a6e4a549
Anton Khorev <tony29@yandex.ru>
2022-11-21 15:55:47 +0300

    Enable sidebar resizing

--- src/index.ts
+++ src/index.ts
@@ -27,4 +27,37 @@ async function main() {
 		}
 	})
 	resizeObserver.observe($ui)
+
+	const pickOriented=(vLandscape:number,vPortrait:number)=>{
+		if ($ui.classList.contains('landscape')) {
+			return vLandscape
+		} else if ($ui.classList.contains('portrait')) {
+			return vPortrait
+		}
+	}
+
+	let resizing=false
+	$resizer.onmousedown=ev=>{
+		const sidebarSize=pickOriented($sidebar.clientWidth,$sidebar.clientHeight)
+		if (sidebarSize==null) return
+		$sidebar.style.flexBasis=sidebarSize+'px'
+		resizing=true
+	}
+	$ui.onmouseup=ev=>{
+		if (!resizing) return
+		ev.stopPropagation()
+		resizing=false
+	}
+	$ui.addEventListener('mousemove',ev=>{
+		if (!resizing) return
+		if (!(ev.buttons&1)) {
+			resizing=false
+			return
+		}
+		ev.stopPropagation()
+		const sidebarSize=pickOriented($sidebar.clientWidth,$sidebar.clientHeight)
+		const dSidebarSize=pickOriented(ev.movementX,ev.movementY)
+		if (sidebarSize==null || dSidebarSize==null) return
+		$sidebar.style.flexBasis=sidebarSize+dSidebarSize+'px'
+	},true)
 }
