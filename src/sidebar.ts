91c7d13750d5d9d97190459fbda0dc82a810df22
Anton Khorev <tony29@yandex.ru>
2022-11-24 13:58:02 +0300

    Make two lead placeholders: one between top buttons, one below; put h1 there

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -1,17 +1,45 @@
 import {makeElement} from './util'
 
+const makeDiv=makeElement('div')
+
 export default class Sidebar {
 	constructor($sidebar: HTMLElement) {
-		const $main=makeElement('main')()(
-			makeElement('h1')()(
-				`Temporary sidebar title`
-			)
+		const $outerLeadPlaceholder=makeDiv('lead','outer')()
+		const $innerLeadPlaceholder=makeDiv('lead','inner')()
+		const $content=makeDiv('content')($innerLeadPlaceholder)
+		$sidebar.append($outerLeadPlaceholder,$content)
+
+		const $heading=makeElement('h1')()(
+			`Temporary sidebar title`
 		)
+
+		const outerLeadWidthBoundary=240
+		let outerLeadMinHeight:number|undefined
+		const outerLeadResizeObserver=new ResizeObserver(()=>{
+			const outerLeadHeight=$outerLeadPlaceholder.clientHeight
+			if (outerLeadMinHeight==null) {
+				outerLeadMinHeight=outerLeadHeight
+			}
+			const outerLeadWidth=$outerLeadPlaceholder.clientWidth
+			if (outerLeadWidth<outerLeadWidthBoundary) {
+				$innerLeadPlaceholder.style.removeProperty('height')
+				$innerLeadPlaceholder.append($heading)
+			} else {
+				$outerLeadPlaceholder.append($heading)
+				const leadHeightDiff=outerLeadHeight-outerLeadMinHeight
+				if (leadHeightDiff>0) {
+					$innerLeadPlaceholder.style.height=leadHeightDiff+'px'
+				} else {
+					$innerLeadPlaceholder.style.removeProperty('height')
+				}
+			}
+		})
+		outerLeadResizeObserver.observe($outerLeadPlaceholder)
+
 		for (let i=0;i<30;i++) {
-			$main.append(makeElement('p')()(
+			$content.append(makeElement('p')()(
 				`Lorem ipsum ${i}!`
 			))
 		}
-		$sidebar.append($main)
 	}
 }
