283f2ba88d8d6a6e7a8cc5cac09716249dba6d33
Anton Khorev <tony29@yandex.ru>
2022-12-11 11:33:06 +0300

    Add test links to places

--- src/modules/test.ts
+++ src/modules/test.ts
@@ -1,5 +1,5 @@
 import Module from '../module'
-import {Content, makeElement} from '../util'
+import {Content, makeElement, makeLink} from '../util'
 
 export default class TestModule extends Module {
 	makeHeading():Content {
@@ -9,6 +9,18 @@ export default class TestModule extends Module {
 	}
 	makeContent():Content {
 		const content:Content=[]
+		const places:[string,string][]=[
+			[`bronze horseman`,`#module=test&map=18/59.93641/30.30218`],
+			[`issac cathedral`,`#module=test&map=18/59.93407/30.30614`],
+			[`winter palace`,`#module=test&map=18/59.94044/30.31377`],
+			[`peter & paul cathedral`,`#module=test&map=18/59.95020/30.31642`],
+			[`mosque`,`#module=test&map=18/59.95520/30.32394`],
+		]
+		for (const [name,href] of places) {
+			content.push(makeElement('p')()(
+				makeLink(name,href)
+			))
+		}
 		for (let i=0;i<30;i++) {
 			content.push(makeElement('p')()(
 				`Lorem ipsum ${i}!`
