423f43ac2db11e6b0e23e8f83393799d570f5202
Anton Khorev <tony29@yandex.ru>
2022-12-15 14:29:21 +0300

    Use power operator

--- src/map/geo.ts
+++ src/map/geo.ts
@@ -5,10 +5,12 @@ export const tileSizePow=8
 export const tileSize=2**tileSizePow
 
 export function calculateX(zoom:number,lon:number):number {
-	return Math.floor(Math.pow(2,zoom+tileSizePow)*(lon+180)/360)
+	const s=2**(zoom+tileSizePow)
+	return Math.floor(s*(lon+180)/360)
 }
 export function calculateY(zoom:number,lat:number):number {
-	return Math.floor(Math.pow(2,zoom+tileSizePow)*(1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2)
+	const s=2**(zoom+tileSizePow)
+	return Math.floor(s*(1-Math.log(Math.tan(lat*Math.PI/180) + 1/Math.cos(lat*Math.PI/180))/Math.PI)/2)
 }
 export function calculatePosition(zoom:number,lat:number,lon:number):Position {
 	return [
@@ -19,11 +21,12 @@ export function calculatePosition(zoom:number,lat:number,lon:number):Position {
 }
 
 export function calculateCoords(x:number,y:number,z:number):Coordinates {
-	const n=Math.PI-2*Math.PI*y/Math.pow(2,z+tileSizePow)
+	const s=2**(z+tileSizePow)
+	const n=Math.PI-2*Math.PI*y/s
 	return [
 		z,
 		180/Math.PI*Math.atan(0.5*(Math.exp(n)-Math.exp(-n))),
-		x/Math.pow(2,z+tileSizePow)*360-180
+		x/s*360-180
 	]
 }
 
